289d83b89b2a8f3f9b85c6d66d8e5b5b9cc15149
paoloviglione <paolo.viglione@unina2.it>
2014-03-18 16:21:28 +0100

    Base

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+        <!-- jQuery e Bootstrap (plugin JavaScript)  -->
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="<?php echo base_url() ?>asset/js/bootstrap.min.js"></script>
+        
+    </body>
+    
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<!-- Classi condizionali con riferimento unicamente a IE8 -->
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en" ><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" ><!--<![endif]-->
+
+    <html>
+
+        <head>
+
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">     
+
+            <meta charset="utf-8">
+
+            <!-- Forza IE ad usare la modalità di rendering più avanzata, dal momento che il framework non supporta la modalità di compatibilità -->
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+            <!-- CSS -->
+            <link href="<?php echo base_url() ?>asset/css/bootstrap.min.css" rel="stylesheet" media="screen">  
+            <link href="<?php echo base_url() ?>asset/css/custom.css" rel="stylesheet" media="screen">              
+
+            <!-- JS Modernizr (consente di applicare una determinata classe ad un oggetto se il browser supporta una determinata proprietà CSS3 o di applicare una classe alternativa in caso contrario) -->
+            <script src="<?php echo base_url() ?>asset/js/modernizr.custom.js"></script>  
+
+            <!-- JS Respond.js per IE8 -->
+            <!--[if lt IE 9]><script src="js/respond.min.js"></script><![endif]-->  
+
+            <title>SUN - Questionario Trasporti</title>
+
+        <body>
+
+            <!-- Header e barra di navigazione -->
+            <header>
+
+                <nav class="navbar navbar-default">
+
+                    <div class="container">
+
+                        <div class="navbar-header">
+
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            
+                            <a class="navbar-brand" href="#">SUN</a>
+                            
+                        </div> 
+
+                        <p class="navbar-text">Questionario di rilevamento del servizio di Trasporto Pubblico</p>
+                        
+                    </div>
+                    
+                    <div class="container">      
+                        
+                        <div class="collapse navbar-collapse navbar-responsive-collapse">
+                            
+                            <ul class="nav navbar-nav bolded">
+
+                                <li>
+                                    <a href="#">Compila</b></a>
+                                </li>
+                                <li>
+                                    <a href="#">FAQ</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contatti</a>
+                                </li>
+                            </ul>
+                        </div><!-- /.nav-collapse -->
+
+                    </div>
+
+                </nav>        
+                
+       
+
+            </header>     
